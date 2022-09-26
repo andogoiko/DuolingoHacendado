7f5dfffe46b3025390a2fa34823a9a762eb987bd
2dam3 <2dam3@HZ114469.H012108.NET>
2022-09-26 13:39:28 +0200

    tema fotos funcional

--- app/src/main/java/com/example/duolingohacendado/MainActivity.java
+++ app/src/main/java/com/example/duolingohacendado/MainActivity.java
@@ -2,9 +2,13 @@ package com.example.duolingohacendado;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.text.Layout;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -14,11 +18,12 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    ImageView imagen;
+    LinearLayout layout;
+    MediaPlayer mpGut, mpNope;
+
+    int posicion = 0;
 
     List<String> IdsIV = new ArrayList<String>(Arrays.asList("IVmoises", "IVskippy", "IVgabriel", "IVisaias"));
-
-
 
     List<ImageView> imagenes = new ArrayList<ImageView>();
 
@@ -26,6 +31,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mpGut = MediaPlayer.create(this, R.raw.sabe_una_cosa);
+        mpNope = MediaPlayer.create(this, R.raw.sabe_una_cosa);
 
         /*mezclamos las respuestas*/
 
@@ -38,40 +46,141 @@ public class MainActivity extends AppCompatActivity {
         imagenes.add(findViewById(R.id.IV3));
         imagenes.add(findViewById(R.id.IV4));
 
-        for (int i = 0; i < 4; i++){
+        for (int i = 0; i < IdsIV.size(); i++){
             switch (IdsIV.get(i)){
 
                 case "IVmoises":
                     imagenes.get(i).setBackgroundResource(R.drawable.moises);
+                    imagenes.get(i).setOnClickListener(new View.OnClickListener(){
+                        @Override
+                        public void onClick(View v) {
+                            opcionIncorrecta();
+                            switch (IdsIV.indexOf("IVmoises")){
+
+                                case 0:
+                                    findViewById(R.id.LIV1).setPadding(5, 5, 5, 5);
+                                    findViewById(R.id.LIV1).setBackgroundColor(Color.RED);
+                                    break;
+
+                                case 1:
+                                    findViewById(R.id.LIV2).setPadding(5, 5, 5, 5);
+                                    findViewById(R.id.LIV2).setBackgroundColor(Color.RED);
+                                    break;
+
+                                case 2:
+                                    findViewById(R.id.LIV3).setPadding(5, 5, 5, 5);
+                                    findViewById(R.id.LIV3).setBackgroundColor(Color.RED);
+                                    break;
+
+                                case 3:
+                                    findViewById(R.id.LIV4).setPadding(5, 5, 5, 5);
+                                    findViewById(R.id.LIV4).setBackgroundColor(Color.RED);
+                                    break;
+                            }
+
+                            mpNope.start();
+                        }
+                    });
                     break;
 
                 case "IVskippy":
                     imagenes.get(i).setBackgroundResource(R.drawable.skippy_el_mensajero_de_dios);
+                    imagenes.get(i).setOnClickListener(new View.OnClickListener(){
+                        @Override
+                        public void onClick(View v) {
+                            opcionCorrecta();
+                            switch (IdsIV.indexOf("IVskippy")){
+
+                                case 0:
+                                    findViewById(R.id.LIV1).setPadding(5, 5, 5, 5);
+                                    findViewById(R.id.LIV1).setBackgroundColor(Color.GREEN);
+                                    break;
+
+                                case 1:
+                                    findViewById(R.id.LIV2).setPadding(5, 5, 5, 5);
+                                    findViewById(R.id.LIV2).setBackgroundColor(Color.GREEN);
+                                    break;
+
+                                case 2:
+                                    findViewById(R.id.LIV3).setPadding(5, 5, 5, 5);
+                                    findViewById(R.id.LIV3).setBackgroundColor(Color.GREEN);
+                                    break;
+
+                                case 3:
+                                    findViewById(R.id.LIV4).setPadding(5, 5, 5, 5);
+                                    findViewById(R.id.LIV4).setBackgroundColor(Color.GREEN);
+                                    break;
+                            }
+                            mpGut.start();
+                        }
+                    });
                     break;
 
                 case "IVgabriel":
                     imagenes.get(i).setBackgroundResource(R.drawable.arcangel_gabriel);
+                    imagenes.get(i).setOnClickListener(new View.OnClickListener(){
+                        @Override
+                        public void onClick(View v) {
+                            opcionIncorrecta();
+                            switch (IdsIV.indexOf("IVgabriel")){
+
+                                case 0:
+                                    findViewById(R.id.LIV1).setPadding(5, 5, 5, 5);
+                                    findViewById(R.id.LIV1).setBackgroundColor(Color.RED);
+                                    break;
+
+                                case 1:
+                                    findViewById(R.id.LIV2).setPadding(5, 5, 5, 5);
+                                    findViewById(R.id.LIV2).setBackgroundColor(Color.RED);
+                                    break;
+
+                                case 2:
+                                    findViewById(R.id.LIV3).setPadding(5, 5, 5, 5);
+                                    findViewById(R.id.LIV3).setBackgroundColor(Color.RED);
+                                    break;
+
+                                case 3:
+                                    findViewById(R.id.LIV4).setPadding(5, 5, 5, 5);
+                                    findViewById(R.id.LIV4).setBackgroundColor(Color.RED);
+                                    break;
+                            }
+                            mpNope.start();
+                        }
+                    });
                     break;
 
                 case "IVisaias":
                     imagenes.get(i).setBackgroundResource(R.drawable.isaias_profeta);
-                    break;
-            }
+                    imagenes.get(i).setOnClickListener(new View.OnClickListener(){
+                        @Override
+                        public void onClick(View v) {
+                            opcionIncorrecta();
+                            switch (IdsIV.indexOf("IVisaias")){
 
-            if (IdsIV.get(i) == "IVskippy"){
-                imagenes.get(i).setOnClickListener(new View.OnClickListener(){
-                    @Override
-                    public void onClick(View v) {
-                        opcionCorrecta();
-                    }
-                });
-            }else{
-                imagenes.get(i).setOnClickListener(new View.OnClickListener(){
-                    @Override
-                    public void onClick(View v) {
-                        opcionIncorrecta();
-                    }
-                });
+                                case 0:
+                                    findViewById(R.id.LIV1).setPadding(5, 5, 5, 5);
+                                    findViewById(R.id.LIV1).setBackgroundColor(Color.RED);
+                                    break;
+
+                                case 1:
+                                    findViewById(R.id.LIV2).setPadding(5, 5, 5, 5);
+                                    findViewById(R.id.LIV2).setBackgroundColor(Color.RED);
+                                    break;
+
+                                case 2:
+                                    findViewById(R.id.LIV3).setPadding(5, 5, 5, 5);
+                                    findViewById(R.id.LIV3).setBackgroundColor(Color.RED);
+                                    break;
+
+                                case 3:
+                                    findViewById(R.id.LIV4).setPadding(5, 5, 5, 5);
+                                    findViewById(R.id.LIV4).setBackgroundColor(Color.RED);
+                                    break;
+                            }
+                            mpNope.start();
+                        }
+                    });
+                    break;
             }
 
 
@@ -80,11 +189,22 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void opcionIncorrecta(){
-        Toast.makeText(MainActivity.this, "¡Incorrecta!", Toast.LENGTH_LONG).show();
+        Toast.makeText(MainActivity.this, "¡Incorrecta!", Toast.LENGTH_SHORT).show();
+
+        for (int i = 0; i < IdsIV.size(); i++){
+            imagenes.get(i).setEnabled(false);
+            imagenes.get(i).setColorFilter(Color.argb(150,0,255,0));
+        }
+
     }
 
     private void opcionCorrecta(){
-        Toast.makeText(MainActivity.this, "¡Correcta!", Toast.LENGTH_LONG).show();
+        Toast.makeText(MainActivity.this, "¡Correcta!", Toast.LENGTH_SHORT).show();
+
+        for (int i = 0; i < IdsIV.size(); i++){
+            imagenes.get(i).setEnabled(false);
+            imagenes.get(i).setColorFilter(Color.argb(150,0,255,0));
+        }
 
     }
 
